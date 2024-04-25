cc1716a7e7892af45b05f9ee2e2771f056849e73
fangzheng <fangzheng@nrp-pet.cn>
2024-04-25 14:18:52 +0800

    first add

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,9 @@
 
 from uvicorn import run
 
+from api import app
+
 # app = create_app()
 
 if __name__ == '__main__':
-    run(host="0.0.0.0", app="main:app", reload=True)
+    run(host="0.0.0.0",app="main:app", reload=True)
