6ff699efb99fc493f8e513ebe9768ab45b0fe2a3
fangzheng <fangzheng@nrp-pet.cn>
2024-04-25 14:59:19 +0800

    first add

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# ANSI 转义序列
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # 启动 gunicorn 服务
 gunicorn main:app -c gunicorn.py --daemon
 
 # 检查 gunicorn 是否成功启动
 if [ $? -eq 0 ]; then
-    echo "Gunicorn started successfully."
+    echo -e "${GREEN}Gunicorn started successfully.${NC}"
 else
-    echo "Failed to start Gunicorn."
+    echo -e "${RED}Failed to start Gunicorn.${NC}"
 fi
