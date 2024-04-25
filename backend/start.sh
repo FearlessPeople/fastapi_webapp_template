af94b0d5a5e7edc035356d118f34d0e726b1f563
fangzheng <fangzheng@nrp-pet.cn>
2024-04-25 14:58:43 +0800

    first add

--- backend/start.sh
+++ backend/start.sh
@@ -1,1 +1,11 @@
-gunicorn main:app -c gunicorn.py
+#!/bin/bash
+
+# 启动 gunicorn 服务
+gunicorn main:app -c gunicorn.py --daemon
+
+# 检查 gunicorn 是否成功启动
+if [ $? -eq 0 ]; then
+    echo "Gunicorn started successfully."
+else
+    echo "Failed to start Gunicorn."
+fi
