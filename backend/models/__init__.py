29209131e7b6e891e720c58ac1e73767a7f0c5e5
fangzheng <fangzheng@nrp-pet.cn>
2024-04-26 11:06:16 +0800

    first add

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,3 +2,4 @@
 
 from .result import Success, Fail, ResponseToken
 from .user import User, User_Pydantic, UserIn_Pydantic
+from .online_user import OnlineUser, OnlineUser_Pydantic, OnlineUserIn_Pydantic
